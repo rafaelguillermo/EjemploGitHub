35385f086da20de064ef57045752672da83a7ff2
Rafael Blanco <mangelan03@gmail.com>
2019-08-29 16:11:30 -0500

    Un comentario

--- app/src/main/java/com/example/ejemplogithub/MainActivity.java
+++ app/src/main/java/com/example/ejemplogithub/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Mensaje
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hola
